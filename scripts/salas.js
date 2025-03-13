bafb44f661f3a296df34859ea1d051ee0b5afad0
Yeray2 <101354040+Yeray2@users.noreply.github.com>
2025-03-13 10:10:56 +0100

    salas y cartas

--- scripts/salas.js
+++ scripts/salas.js
@@ -1,15 +1,35 @@
+$(document).ready(function () {
+    $('.multiple-card-slider .carousel').each(function () {
+        var currentCarouselId = $(this).attr('id');
+        const multipleItemCarousel = document.getElementById(currentCarouselId);
 
+        if (window.matchMedia("(min-width:576px)").matches) {
+            const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+                interval: false,
+                wrap: false
+            });
 
-document.addEventListener('DOMContentLoaded', () => {
-    let but = document.getElementById("cohetes");
-    but.addEventListener("click", () => mostrartabla());
-    //más listeners
-    })
+            var carouselWidth = $('#' + currentCarouselId + ' .carousel-inner')[0].scrollWidth;
+            var cardWidth = $('#' + currentCarouselId + ' .carousel-item').outerWidth(true);
+            var scrollPosition = 0;
 
+            $('#' + currentCarouselId + ' .carousel-control-next').on('click', function () {
+                if (scrollPosition < (carouselWidth - (cardWidth * 4))) {
+                    console.log('next');
+                    scrollPosition += cardWidth;
+                    $('#' + currentCarouselId + ' .carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+                }
+            });
 
-function mostrartabla() {
-
-
-    
-
-}
+            $('#' + currentCarouselId + ' .carousel-control-prev').on('click', function () {
+                if (scrollPosition > 0) {
+                    console.log('prev');
+                    scrollPosition -= cardWidth;
+                    $('#' + currentCarouselId + ' .carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+                }
+            });
+        } else {
+            $(multipleItemCarousel).addClass('slide');
+        }
+    });
+});
